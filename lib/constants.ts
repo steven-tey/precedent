dac14351c20df35401962266da1dd0ead750d228
Steven Tey <stevensteel97@gmail.com>
2023-01-13 08:38:23 -0600

    updated env vars

--- lib/constants.ts
+++ lib/constants.ts
@@ -16,4 +16,4 @@ export const FADE_UP_ANIMATION_VARIANTS = {
 };
 
 export const DEPLOY_URL =
-  "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsteven-tey%2Fprecedent&project-name=precedent&repository-name=precedent&demo-title=Precedent&demo-description=An%20opinionated%20collection%20of%20components%2C%20hooks%2C%20and%20utilities%20for%20your%20Next%20project.&demo-url=https%3A%2F%2Fprecedent.vercel.app&demo-image=https%3A%2F%2Fprecedent.vercel.app%2Fapi%2Fog";
+  "https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsteven-tey%2Fprecedent&project-name=precedent&repository-name=precedent&demo-title=Precedent&demo-description=An%20opinionated%20collection%20of%20components%2C%20hooks%2C%20and%20utilities%20for%20your%20Next%20project.&demo-url=https%3A%2F%2Fprecedent.vercel.app&demo-image=https%3A%2F%2Fprecedent.vercel.app%2Fapi%2Fog&env=DATABASE_URL,GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET,NEXTAUTH_SECRET&envDescription=How%20to%20get%20these%20env%20variables%3A&envLink=https%3A%2F%2Fgithub.com%2Fsteven-tey%2Fprecedent%2Fblob%2Fmain%2F.env.example";
