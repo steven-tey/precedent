6ad4ef994a284b518d5d8e3be4b73961796fdd1c
Steven Tey <stevensteel97@gmail.com>
2023-01-15 19:13:26 -0600

    added redirect to github

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/steven-tey/precedent",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
