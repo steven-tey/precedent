44da661c88358d05be41b99e4369cde04bb4472a
Steven Tey <stevensteel97@gmail.com>
2023-01-16 23:00:34 -0600

    Update sign-in-modal.tsx

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -21,7 +21,7 @@ const SignInModal = ({
 
   return (
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
-      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-100">
+      <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
           <a href="https://precedent.dev">
             <Image
