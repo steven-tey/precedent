5642a0d85494899262f5b7020640d564ec9df264
Steven Tey <stevensteel97@gmail.com>
2023-04-18 11:20:17 -0500

    fix signout

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -5,11 +5,9 @@ import { signOut } from "next-auth/react";
 import { LayoutDashboard, LogOut } from "lucide-react";
 import Popover from "@/components/shared/popover";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 
 export default function UserDropdown({ session }: { session: Session }) {
-  const router = useRouter();
   const { email, image } = session?.user || {};
   const [openPopover, setOpenPopover] = useState(false);
 
@@ -36,7 +34,7 @@ export default function UserDropdown({ session }: { session: Session }) {
             </button>
             <button
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
-              onClick={() => signOut}
+              onClick={() => signOut()}
             >
               <LogOut className="h-4 w-4" />
               <p className="text-sm">Logout</p>
