ac8e4af4db472366fb4b18fe544d4bf7a198e7c9
Steven Tey <stevensteel97@gmail.com>
2023-05-17 12:14:15 -0500

    upgrade next to 13.4, removed experimental.appDir

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
