ad9dd2b2844d419dacbaefe775459dcd9d6d2655
Steven Tey <stevensteel97@gmail.com>
2023-10-16 13:15:05 -0700

    remove twitter field

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,6 @@ export const metadata = {
   title: "Precedent - Building blocks for your Next.js project",
   description:
     "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-  twitter: {
-    card: "summary_large_image",
-    title: "Precedent - Building blocks for your Next.js project",
-    description:
-      "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    creator: "@steventey",
-  },
   metadataBase: new URL("https://precedent.dev"),
   themeColor: "#FFF",
 };
