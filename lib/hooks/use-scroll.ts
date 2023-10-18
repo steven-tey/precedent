eb59ec64d84adda2ce87d8e97c4d46e8ff76d8b0
KentHHarris <kenthiharris@gmail.com>
2023-10-18 15:37:23 -0700

    [Fix] Correctly set scrolled state on first render

--- lib/hooks/use-scroll.ts
+++ lib/hooks/use-scroll.ts
@@ -8,6 +8,8 @@ export default function useScroll(threshold: number) {
   }, [threshold]);
 
   useEffect(() => {
+    window && onScroll(); // Call on first render. This correctly updates the `scrolled` state to `True` if the user's scroll position is greater than the `threshold` during the initial load.
+
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, [onScroll]);
