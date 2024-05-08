e1925cf003166e3ed96b42d4c57f03d04222dadf
Steven Tey <stevensteel97@gmail.com>
2024-05-08 23:15:03 +0300

    fix 2

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,42 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { cookies } from "next/headers";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  events: {
+    signIn: async ({ user, isNewUser }) => {
+      if (isNewUser || process.env.NODE_ENV === "development") {
+        const { id } = user;
+
+        const clickId = cookies().get("dclid")?.value;
+
+        if (clickId) {
+          console.log("clickId detected: ", clickId);
+
+          await fetch("https://api-staging.dub.co/track", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${process.env.DUB_API_KEY}`,
+            },
+            body: JSON.stringify({
+              clickId,
+              eventName: "Created an account",
+              eventType: "lead",
+              customerId: id,
+            }),
+          }).then((res) => res.json());
+        }
+      }
+    },
+  },
+};
