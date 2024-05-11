50aa41d6b4c977a4a5e37436d2681ac19cc83766
Steven Tey <stevensteel97@gmail.com>
2024-05-11 20:08:34 +0300

    update version

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,32 +1,14 @@
 "use client";
 
 import { Analytics } from "@vercel/analytics/react";
-import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
 import { Analytics as DubAnalytics } from "@dub/analytics/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Suspense>
-        <SetDubCookie />
-      </Suspense>
+      <DubAnalytics />
       {children}
       <Analytics />
-      <DubAnalytics apiKey="test" />
     </>
   );
 }
-
-const SetDubCookie = () => {
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const dclid = searchParams.get("dclid");
-    if (dclid) {
-      // set cookie
-      document.cookie = `dclid=${dclid}; path=/; max-age=31536000`;
-    }
-  }, [searchParams]);
-  return null;
-};
