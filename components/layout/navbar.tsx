3680d6bce80fa5db7aba745ce6d791b4be94328a
Steven Tey <stevensteel97@gmail.com>
2024-09-30 17:52:09 -0700

    fix sign in button

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -12,13 +12,13 @@ export default function NavBar() {
   return (
     <>
       <div
-        className={`fixed top-0 w-full flex justify-center ${
+        className={`fixed top-0 flex w-full justify-center ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
             : "bg-white/0"
         } z-30 transition-all`}
       >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
+        <div className="mx-5 flex h-16 w-full max-w-screen-xl items-center justify-between">
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
@@ -30,7 +30,11 @@ export default function NavBar() {
             <p>Precedent</p>
           </Link>
           <SignedOut>
-            <SignInButton mode="modal" />
+            <SignInButton mode="modal">
+              <button className="rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white transition-colors hover:bg-white hover:text-black">
+                Sign In
+              </button>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton>
@@ -38,7 +42,8 @@ export default function NavBar() {
                 <UserButton.Link
                   label="Dashboard"
                   labelIcon={<LayoutDashboard className="h-4 w-4" />}
-                  href="/" />
+                  href="/"
+                />
               </UserButton.MenuItems>
             </UserButton>
           </SignedIn>
